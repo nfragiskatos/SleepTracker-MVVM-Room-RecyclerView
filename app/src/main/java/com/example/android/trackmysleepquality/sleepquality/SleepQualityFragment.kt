6c5bbb3fd4c5120fe00069e257dac5e277ab47b6
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-19 15:44:44 -0700

    - Boilerplate code to get access to the SleepQualityViewModel in the fragment

--- app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityFragment.kt
@@ -22,7 +22,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.databinding.FragmentSleepQualityBinding
 
 /**
@@ -46,6 +48,15 @@ class SleepQualityFragment : Fragment() {
                 inflater, R.layout.fragment_sleep_quality, container, false)
 
         val application = requireNotNull(this.activity).application
+
+        // Get the arguments that came with the navigation when coming here from Sleep Quality fragment.
+        val arguments = SleepQualityFragmentArgs.fromBundle(arguments!!)
+
+        val sleepDatabaseDao = SleepDatabase.getInstance(application).sleepDatabaseDao
+
+        val viewModelFactory = SleepQualityViewModelFactory(arguments.sleepNightKey, sleepDatabaseDao)
+
+        val sleepQualityViewModel = ViewModelProvider(this, viewModelFactory).get(SleepQualityViewModel::class.java)
 
         return binding.root
     }
