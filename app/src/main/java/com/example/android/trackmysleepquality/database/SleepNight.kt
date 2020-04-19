1ffb46a4fd18befafc2988ea0f7cbb2594cc10b8
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-19 07:42:20 -0700

    - Created SleepNight data class that will act as our entity for Room to use

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -15,3 +15,10 @@
  */
 
 package com.example.android.trackmysleepquality.database
+
+data class SleepNight(
+        var nightId: Long = 0L,
+        var startTimeMilli: Long = System.currentTimeMillis(),
+        var endTimeMilli: Long = startTimeMilli,
+        var sleepQuality: Int = -1
+)
