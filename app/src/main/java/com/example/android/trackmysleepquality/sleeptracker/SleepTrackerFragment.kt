0f502228cb56a937dc9670c6cbf983f2ec75b90a
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-19 13:20:05 -0700

    - Added view model variable to xml layout - Added all boiler plate code to SleepTrackerFragment needed to build up and get a SleepTrackerViewModel from the ViewModelProvider. - Ensured our binding for our fragment has its view model variable set, as well as its lifecycle owner

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -22,7 +22,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.databinding.FragmentSleepTrackerBinding
 
 /**
@@ -43,6 +45,18 @@ class SleepTrackerFragment : Fragment() {
         // Get a reference to the binding object and inflate the fragment views.
         val binding: FragmentSleepTrackerBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_sleep_tracker, container, false)
+
+        /*
+        Build all these just to build the view model from the factory
+         */
+        val application = requireNotNull(this.activity).application
+        val sleepDbDao = SleepDatabase.getInstance(application).sleepDatabaseDao
+        val viewModelFactory = SleepTrackerViewModelFactory(sleepDbDao, application)
+
+        val sleepTrackerViewModel = ViewModelProvider(this, viewModelFactory).get(SleepTrackerViewModel::class.java)
+
+        binding.sleepTrackerViewModel = sleepTrackerViewModel
+        binding.lifecycleOwner = this
 
         return binding.root
     }
