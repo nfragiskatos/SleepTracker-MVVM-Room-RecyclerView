74da65a503075245fb291194ad235f2127d30334
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-19 08:12:34 -0700

    - Created our abstract SleepDatabase that will extend RoomDatabase (Room will create the implementation for us) - Starting with version 1 - Right now there is only 1 entity (SleepNight)

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -15,3 +15,11 @@
  */
 
 package com.example.android.trackmysleepquality.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase: RoomDatabase() {
+
+}
