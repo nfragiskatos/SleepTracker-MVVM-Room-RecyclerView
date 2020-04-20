988d0fc5f06f5ec8ca96632706ccec1350d5f6c6
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-20 15:22:25 -0700

    - implemented onCreateViewHolder to tell the adapter how to create new ViewHolders - Added a custom setter on our backing data list, so when it gets updated we can call notifyDataSetChanged to tell the RecyclerView to redraw everything to accommodate the new data. This completely clobbers everything and redraws all views even if they don't need to be redrawn. There are better ways to do this.

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -1,7 +1,10 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.TextItemViewHolder
 import com.example.android.trackmysleepquality.database.SleepNight
 
@@ -9,10 +12,22 @@ import com.example.android.trackmysleepquality.database.SleepNight
  * Created by Nicholas Fragiskatos on 4/20/2020.
  */
 class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
+
+    /*
+    Want to provider a custom set that sets the new value, but also notifies the RecyclerView that
+    the data has changed so it can redraw everything on screen based on the new data.
+    ** Not the best approach though.
+     */
     var data = listOf<SleepNight>()
+    set(value) {
+        field = value
+        notifyDataSetChanged()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
-        TODO("Not yet implemented")
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.text_item_view, parent, false) as TextView
+        return TextItemViewHolder(view)
     }
 
     override fun getItemCount(): Int = data.size
