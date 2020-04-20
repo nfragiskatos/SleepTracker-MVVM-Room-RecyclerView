1fb0670bcdeec7beee3f2b6241f9b285e8ba3fe1
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-20 12:47:21 -0700

    - Added dependencies - Created bare bones adapter for SleepNight

---
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.android.trackmysleepquality.sleeptracker
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.trackmysleepquality.TextItemViewHolder
+
+/**
+ * Created by Nicholas Fragiskatos on 4/20/2020.
+ */
+class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
