0cceed72b7c23470e385786209a4ed3fa8bcc5a5
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-20 12:51:00 -0700

    - Implemented the getItemCount - Fake implementation of onBindViewHolder

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -3,21 +3,23 @@ package com.example.android.trackmysleepquality.sleeptracker
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.TextItemViewHolder
+import com.example.android.trackmysleepquality.database.SleepNight
 
 /**
  * Created by Nicholas Fragiskatos on 4/20/2020.
  */
 class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
+    var data = listOf<SleepNight>()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
         TODO("Not yet implemented")
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = data.size
 
     override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = data[position]
+        holder.textView.text = item.sleepQuality.toString()
     }
 
 }
