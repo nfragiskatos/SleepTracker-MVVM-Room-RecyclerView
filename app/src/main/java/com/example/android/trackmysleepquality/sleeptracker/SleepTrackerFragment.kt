4454951464be631db32baf342cdb6a467adac59e
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-22 14:54:21 -0700

    - Now when we new up a new instance of the SleepNightAdapter we have to pass the constructor a SleepNightListener. Created a SleepNightListener instance which just makes a toast message for now.

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -66,7 +67,9 @@ class SleepTrackerFragment : Fragment() {
         Here we want to take our RecyclerView in our resource file (fragment_sleep_tracker.xml) and
         set its adapter to our custom adapter so it knows what to display.
          */
-        val adapter = SleepNightAdapter()
+        val adapter = SleepNightAdapter(SleepNightListener { nightId ->
+            Toast.makeText(context, "$nightId", Toast.LENGTH_LONG).show()
+        })
         binding.sleepList.adapter = adapter
 
         /*
