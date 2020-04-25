09835e27916c3c1d4042e305dc6646075c25d1d2
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-25 07:40:19 -0700

    - We can't submit the list directly now because we need to wrap the List<SleepNight> as a List<DataItem> instead plus add a header, so we use this wrapper function we created in the SleepNightAdapter, which will end up submitting the list for us.

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -20,7 +20,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -85,7 +84,7 @@ class SleepTrackerFragment : Fragment() {
          */
         sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
             it?.let {
-                adapter.submitList(it)
+                adapter.addHeaderAndSubmitList(it)
             }
         })
 
